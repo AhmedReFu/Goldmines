dc6d2c609a1298be5deaf7306b6925ed25b9ad38
AhmedReFu <mdnazmulkhanfeni@gmil.com>
2025-01-24 10:33:44 +0600

    some chages

--- components/Footer.js
+++ components/Footer.js
@@ -13,7 +13,8 @@ const Footer = () => {
                                 <Link href='/'>Goldmines Picture</Link>
 
                             </h1>
-                            <h1 className=''>Copyright © {year} - All right reserved by <span> <Link href='/'>Goldmines Picture</Link></span></h1>
+                            <a href="https://www.dmca.com/Protection/Status.aspx?ID=6da34590-5510-4620-a257-c6a019d089bf&refurl=https://goldminespicture.xyz/" title="DMCA.com Protection Status" class="dmca-badge"> <img src="https://images.dmca.com/Badges/dmca_protected_sml_120m.png?ID=6da34590-5510-4620-a257-c6a019d089bf" alt="DMCA.com Protection Status" /></a>  <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
+                            <p className=''>Copyright © {year} - All right reserved by Goldmines Picture</p>
                         </div>
                     </div>
                 </section>

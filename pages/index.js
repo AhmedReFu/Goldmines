2520e2b139abb7501da8dff3ffe88da3c1478c20
AhmedReFu <mdnazmulkhanfeni@gmil.com>
2025-02-22 21:17:19 +0600

    wow

--- pages/index.js
+++ pages/index.js
@@ -96,6 +96,7 @@ export default function Home() {
         <meta name='dmca-site-verification' content='WEVvQy9xdXROYXhiSE52UlVBWlBuUmV3MmpLcDRvRmpRaklxTEZtMVZNST01' />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="google-site-verification" content="b3EW3fiqt7pg5pyibUuJtz_cGIqeOOrSke6hxx3tzD4" />
         <script type='text/javascript' src='//pl17402522.effectiveratecpm.com/d9/16/02/d9160275c67db3687e67e1710f806ea8.js'></script>
       </Head>
 

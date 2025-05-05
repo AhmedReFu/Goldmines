a9ac38093ecd9cb7224dea0668a0c26b9f14748a
AhmedReFu <mdnazmulkhanfeni@gmil.com>
2025-05-05 19:32:09 +0600

    Google Tag

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,16 @@ export default function Document() {
 
       <Head>
 
-
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-1T2SLM58J8"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1T2SLM58J8');
+        </script>
+
+
 
       </Head>
       <body>

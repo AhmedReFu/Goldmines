6da8d8afe71db9baad098ad611c8503001087a74
AhmedReFu <mdnazmulkhanfeni@gmil.com>
2025-05-05 20:11:26 +0600

    google analatics add

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return <>
     <Header/>
     <main >
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-1T2SLM58J8`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', 'G-1T2SLM58J8', {
+             page_path: window.location.pathname,
+           });
+         `,
+        }}
+      />
       <Component {...pageProps} />
     </main>
     <Footer />

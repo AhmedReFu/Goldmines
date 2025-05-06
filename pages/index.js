05a4fc366ce574642bdb538232619c1eb1257750
AhmedReFu <mdnazmulkhanfeni@gmil.com>
2025-05-06 11:32:45 +0600

    Seo

--- pages/index.js
+++ pages/index.js
@@ -97,19 +97,6 @@ export default function Home() {
     <>
       <Head>
         <title>Goldmines Picture</title>
-        <meta name="description" content="Watch BollyWood & HollyWood Dubbed Movies, TV Series in Hindi Dubbed, Dual Audio | All Movies,  TV Show , South Movies In Hindi + English Subtitles | HEVC | x264 300mb | Genres: BollyWood, HollyWood, Dual Audio, South Movies, Free Download" />
-        <meta name='dmca-site-verification' content='WEVvQy9xdXROYXhiSE52UlVBWlBuUmV3MmpLcDRvRmpRaklxTEZtMVZNST01' />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-
-        <meta property="og:image" content="https://goldminespicture.xyz/goldminespicture.jpg" />
-
-        <meta property="type" content="website" />
-        <meta name="google-site-verification" content="b3EW3fiqt7pg5pyibUuJtz_cGIqeOOrSke6hxx3tzD4" />
-
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1T2SLM58J8"></script>
-
-
         <script type='text/javascript' src='//pl17402522.effectiveratecpm.com/d9/16/02/d9160275c67db3687e67e1710f806ea8.js'></script>
       </Head>
 

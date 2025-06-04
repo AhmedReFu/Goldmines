7b560adec74bcf01c19660bd8327954437583448
MD NAZMUL ISLAM <59439125+AhmedReFu@users.noreply.github.com>
2025-06-05 04:12:42 +0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,43 +1,96 @@
-import { Head, Html, Main, NextScript, Script } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-
       <Head>
-
-        <meta name="description" content="Goldmines Picture | Download Dual Audio Hindi Dubbed Movies, BollyWood & HollyWood Dubbed Movies, TV Series in Hindi Dubbed, Dual Audio | All Movies, TV Show, South Movies In Hindi + English Subtitles " />
-
+        <meta name="description" content="Goldmines Picture | Download Dual Audio Hindi Dubbed Movies, BollyWood & HollyWood Dubbed Movies, TV Series in Hindi Dubbed, Dual Audio | All Movies, TV Show, South Movies In Hindi + English Subtitles" />
         <meta name="keywords" content="Goldmines, Picture, South Movies Hindi Download, 2731197, Hindi Dubbed Movie Download" />
-
-        <meta name='dmca-site-verification' content='WEVvQy9xdXROYXhiSE52UlVBWlBuUmV3MmpLcDRvRmpRaklxTEZtMVZNST01' />
-        <meta name="google-adsense-account" content="ca-pub-4392057202464130">
-
+        <meta name="dmca-site-verification" content="WEVvQy9xdXROYXhiSE52UlVBWlBuUmV3MmpLcDRvRmpRaklxTEZtMVZNST01" />
+        <meta name="google-adsense-account" content="ca-pub-4392057202464130" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-
         <link rel="icon" href="/favicon.ico" sizes="any" />
-
         <meta property="og:title" content="Goldmines Picture | Download Dual Audio Hindi Dubbed Movies" />
-        <meta property="og:description" content="Goldmines Picture Download Movie BollyWood & HollyWood Dubbed Movies, TV Series in Hindi Dubbed, Dual Audio | All Movies, TV Show, South Movies In Hindi + English Subtitles"/>
+        <meta property="og:description" content="Goldmines Picture Download Movie BollyWood & HollyWood Dubbed Movies, TV Series in Hindi Dubbed, Dual Audio | All Movies, TV Show, South Movies In Hindi + English Subtitles" />
         <meta property="og:image" content="https://goldminespicture.xyz/goldminespicture.jpg" />
         <meta property="og:url" content="https://goldminespicture.xyz" />
         <meta property="og:type" content="website" />
-
-        <meta property="type" content="website" />
         <meta name="google-site-verification" content="b3EW3fiqt7pg5pyibUuJtz_cGIqeOOrSke6hxx3tzD4" />
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1T2SLM58J8"></script>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4392057202464130"
-     crossorigin="anonymous"></script>
-        <script data-cfasync='false' src='//wwr.hlinit.com/?tag=3002c935'></script>
-        <script src="https://fpyf8.com/88/tag.min.js" data-zone="150224" async data-cfasync="false"></script>
-       
-       </Head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-1T2SLM58J8"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1T2SLM58J8');
+            `,
+          }}
+        />
+
+        {/* Google AdSense */}
+        <Script
+          async
+          crossOrigin="anonymous"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4392057202464130"
+          strategy="afterInteractive"
+        />
+
+        {/* Third-party scripts */}
+        <Script
+          src="//wwr.hlinit.com/?tag=3002c935"
+          data-cfasync="false"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="https://fpyf8.com/88/tag.min.js"
+          data-zone="150224"
+          data-cfasync="false"
+          strategy="afterInteractive"
+        />
+
+        {/* Inline dynamic scripts */}
+        <Script
+          id="dynamic-script-1"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d,z,s){
+                s.src='https://'+d+'/401/'+z;
+                try {
+                  (document.body||document.documentElement).appendChild(s);
+                } catch(e){
+                  console.error(e);
+                }
+              })('groleegni.net',9415717,document.createElement('script'));
+            `,
+          }}
+        />
+        <Script
+          id="dynamic-script-2"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(s,u,z,p){
+                s.src=u;
+                s.setAttribute('data-zone',z);
+                (p||document.body||document.documentElement).appendChild(s);
+              })(document.createElement('script'),'https://al5sm.com/tag.min.js',9415718,document.body||document.documentElement);
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
-        
-        
       </body>
     </Html>
   );
